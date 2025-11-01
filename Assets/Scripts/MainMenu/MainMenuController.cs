c2308f4a4addf2d2b1818b958dc146ed71761ba2
ftn000 <kuznecovegor29@gmail.com>
2025-11-01 19:46:57 +0300

    MainMenuAdded

---
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName = "GameTest";
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject buttonPanel;
+
+    public void PlayGame()
+    {
+        Debug.Log("[MainMenu] Starting game...");
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OpenSettings()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(true);
+            buttonPanel.SetActive(false);
+
+        }
+    }
+
+    public void CloseSettings()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(false);
+            buttonPanel.SetActive(true);
+
+        }
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("[MainMenu] Exiting game...");
+        Application.Quit();
+
+        // В редакторе Unity не работает Quit — для теста:
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
